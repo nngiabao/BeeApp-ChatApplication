5c8404d2d6801107ac4b1df2cca2cc951053d241
Bao <nngiabao4@gmail.com>
2025-11-28 13:32:20 -0500

    Update Sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import SidebarNav from "./SidebarNav";
-import ChatList from "./ChatListItem";
+import ChatList from "./Chatlist";
 import ContactList from "../contact/ContactList";
 import SettingsPanel from "../Setting/SettingPanel";
 
